738321df9bf769cffa082693584778b172b2d899
Armin Häberling <armin.aha@gmail.com>
2012-06-21 16:59:46 +0200

    Add DigialClock widget with TimeZone support.

---
+++ worldclockwidget/src/ch/corten/aha/widget/DigitalClock.java
@@ -0,0 +1,127 @@
+package ch.corten.aha.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * Like AnalogClock, but digital.  Shows seconds.
+ *
+ * FIXME: implement separate views for hours/minutes/seconds, so
+ * proportional fonts don't shake rendering
+ */
+public class DigitalClock extends TextView {
+
+    Calendar mCalendar;
+    private final static String m12 = "h:mm:ss aa";
+    private final static String m24 = "k:mm:ss";
+    private FormatChangeObserver mFormatChangeObserver;
+
+    private Runnable mTicker;
+    private Handler mHandler;
+    
+    private boolean mTickerStopped = false;
+
+    private String mFormat;
+    private TimeZone mTimeZone;
+
+    public DigitalClock(Context context) {
+        super(context);
+        initClock(context);
+    }
+
+    public DigitalClock(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initClock(context);
+    }
+    
+    public TimeZone getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(TimeZone timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    private void initClock(Context context) {
+        Resources r = context.getResources();
+
+        if (mCalendar == null) {
+            mCalendar = Calendar.getInstance();
+        }
+
+        mFormatChangeObserver = new FormatChangeObserver();
+        getContext().getContentResolver().registerContentObserver(
+                Settings.System.CONTENT_URI, true, mFormatChangeObserver);
+
+        setFormat();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        mTickerStopped = false;
+        super.onAttachedToWindow();
+        mHandler = new Handler();
+
+        /**
+         * requests a tick on the next hard-second boundary
+         */
+        mTicker = new Runnable() {
+                public void run() {
+                    if (mTickerStopped) return;
+                    mCalendar.setTimeInMillis(System.currentTimeMillis());
+                    java.text.DateFormat df = new SimpleDateFormat(mFormat);
+                    if (mTimeZone != null) {
+                        df.setTimeZone(mTimeZone);
+                    }
+                    setText(df.format(mCalendar));
+                    invalidate();
+                    long now = SystemClock.uptimeMillis();
+                    long next = now + (1000 - now % 1000);
+                    mHandler.postAtTime(mTicker, next);
+                }
+            };
+        mTicker.run();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mTickerStopped = true;
+    }
+
+    /**
+     * Pulls 12/24 mode from system settings
+     */
+    private boolean get24HourMode() {
+        return android.text.format.DateFormat.is24HourFormat(getContext());
+    }
+
+    private void setFormat() {
+        if (get24HourMode()) {
+            mFormat = m24;
+        } else {
+            mFormat = m12;
+        }
+    }
+
+    private class FormatChangeObserver extends ContentObserver {
+        public FormatChangeObserver() {
+            super(new Handler());
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            setFormat();
+        }
+    }
+}
