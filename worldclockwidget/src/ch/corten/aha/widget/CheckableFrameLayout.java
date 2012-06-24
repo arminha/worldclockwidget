e6f609a7f3a16c5a519cd5991615b8691e713ddc
Armin Häberling <armin.aha@gmail.com>
2012-06-24 12:16:07 +0200

    Fix selection background on pre Honeycomb devices.

---
+++ worldclockwidget/src/ch/corten/aha/widget/CheckableFrameLayout.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2012  Armin Häberling
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package ch.corten.aha.widget;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+import android.widget.FrameLayout;
+
+public class CheckableFrameLayout extends FrameLayout implements Checkable {
+    private boolean mChecked;
+    
+    public CheckableFrameLayout(Context context, AttributeSet attrs,
+            int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CheckableFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckableFrameLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isChecked() {
+        return mChecked;
+    }
+    
+    @Override
+    public void setChecked(boolean checked) {
+        mChecked = checked;
+        if (checked) {
+            Drawable drawable = getContext().getResources().getDrawable(com.actionbarsherlock.R.drawable.abs__list_activated_holo);
+            setBackgroundDrawable(drawable);
+        } else {
+            setBackgroundDrawable(new ColorDrawable(android.R.color.transparent));
+        }
+    }
+    
+    @Override
+    public void toggle() {
+        setChecked(!mChecked);
+    }
+}
