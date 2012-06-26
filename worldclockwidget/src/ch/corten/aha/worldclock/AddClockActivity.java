49337c754eaf45a7a2fd871d555686820605f18a
Armin Häberling <armin.aha@gmail.com>
2012-06-26 17:12:24 +0200

    Use new city data in AddClockActivity.

--- worldclockwidget/src/ch/corten/aha/worldclock/AddClockActivity.java
+++ worldclockwidget/src/ch/corten/aha/worldclock/AddClockActivity.java
@@ -16,9 +16,8 @@
 
 package ch.corten.aha.worldclock;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.TimeZone;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -28,23 +27,28 @@ import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuItem.OnActionExpandListener;
 
-import ch.corten.aha.widget.FilterableArrayAdapter;
+import ch.corten.aha.worldclock.R.string;
 import ch.corten.aha.worldclock.provider.WorldClock;
+import ch.corten.aha.worldclock.provider.WorldClock.Cities;
+import android.content.ContentUris;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.ResourceCursorAdapter;
 import android.support.v4.widget.SearchViewCompat;
 import android.support.v4.widget.SearchViewCompat.OnQueryTextListenerCompat;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.util.SparseBooleanArray;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -72,22 +76,73 @@ public class AddClockActivity extends SherlockFragmentActivity {
         return true;
     }
     
-    public static class TimeZoneListFragment extends SherlockListFragment {
-        private ArrayAdapter<TimeZoneInfo> mAdapter;
+    public static class TimeZoneListFragment extends SherlockListFragment implements
+            LoaderManager.LoaderCallbacks<Cursor> {
+        private CursorAdapter mAdapter;
         private View mSearchView;
         private MenuItem mSearchItem;
+        private String mCurFilter;
 
+        private static final String[] CITY_PROJECTION = {
+            Cities._ID,
+            Cities.NAME,
+            Cities.COUNTRY_CODE,
+            Cities.TIMEZONE_ID
+            };
+        
         @Override
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
             setHasOptionsMenu(true);
             
-            mAdapter = new TimeZoneArrayAdapter(getActivity(),
-                    TimeZoneInfo.getAllTimeZones());
-            mAdapter.sort(new CityComparer());
+            mAdapter = new ResourceCursorAdapter(getActivity(), R.layout.time_zone_item, null) {
+                
+                @Override
+                public void bindView(View view, Context context, Cursor cursor) {
+                    TextView cityText = (TextView) view.findViewById(R.id.city_text);
+                    cityText.setText(cursor.getString(cursor.getColumnIndex(Cities.NAME)));
+                    TextView areaText = (TextView) view.findViewById(R.id.area_text);
+                    String countryCode = cursor.getString(cursor.getColumnIndex(Cities.COUNTRY_CODE));
+                    areaText.setText(getCountryName(countryCode));
+                    TextView timeDiffText = (TextView) view.findViewById(R.id.time_diff_text);
+                    TimeZone tz = TimeZone.getTimeZone(cursor.getString(cursor.getColumnIndex(Cities.TIMEZONE_ID)));
+                    timeDiffText.setText(TimeZoneInfo.getTimeDifferenceString(tz));
+                    TextView timeZoneDescText = (TextView) view.findViewById(R.id.timezone_desc_text);
+                    timeZoneDescText.setText(TimeZoneInfo.getDescription(tz));
+                }
+            };
             setListAdapter(mAdapter);
+            setListShown(false);
+            getLoaderManager().initLoader(0, null, this);
         }
 
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            String selection = null;
+            if (!TextUtils.isEmpty(mCurFilter)) {
+                selection = Cities.ASCII_NAME + " like '%" + mCurFilter + "%' or " 
+                        + Cities.NAME + " like '%" + mCurFilter + "%'"; 
+            }
+            return new CursorLoader(getActivity(), Cities.CONTENT_URI,
+                    CITY_PROJECTION, selection, null, null);
+        }
+        
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            mAdapter.changeCursor(null);
+        }
+        
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            mAdapter.changeCursor(data);
+            // The list should now be shown.
+            if (isResumed()) {
+                setListShown(true);
+            } else {
+                setListShownNoAnimation(true);
+            }
+        }
+        
         private void setupCab() {
 //            ListView listView = getListView();
 //            listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
@@ -210,40 +265,49 @@ public class AddClockActivity extends SherlockFragmentActivity {
             }
         }
 
+        private static final String[] ADD_CITY_PROJECTION = {
+            Cities.NAME,
+            Cities.LATITUDE,
+            Cities.LONGITUDE,
+            Cities.COUNTRY_CODE,
+            Cities.TIMEZONE_ID
+        };
+        
         @Override
         public void onListItemClick(ListView l, View v, int position, long id) {
             super.onListItemClick(l, v, position, id);
-            final TimeZoneInfo timeZone = mAdapter.getItem(position);
-            WorldClock.Clocks.addClock(getActivity(), timeZone);
+            Uri uri = ContentUris.withAppendedId(Cities.CONTENT_URI, id);
+            Cursor c = getActivity().getContentResolver().query(uri, ADD_CITY_PROJECTION, null, null, null);
+            c.moveToNext();
+            String timeZoneId = c.getString(c.getColumnIndex(Cities.TIMEZONE_ID));
+            String city = c.getString(c.getColumnIndex(Cities.NAME));
+            String countryCode = c.getString(c.getColumnIndex(Cities.COUNTRY_CODE));
+            String country = getCountryName(countryCode);
+            int timeDiff = TimeZoneInfo.getTimeDifference(TimeZone.getTimeZone(timeZoneId));
+            WorldClock.Clocks.addClock(getActivity(), timeZoneId, city, country, timeDiff);
             returnResult(1);
         }
         
-        private void addSelected() {
-            final SparseBooleanArray positions = getListView().getCheckedItemPositions();
-            final ArrayList<TimeZoneInfo> timeZones = new ArrayList<TimeZoneInfo>();
-            for (int i = 0; i < mAdapter.getCount(); i++) {
-                if (positions.get(i)) {
-                    timeZones.add(mAdapter.getItem(i));
-                }
+        private String getCountryName(String isoCode) {
+            try {
+                Class<string> c = R.string.class;
+                Field field = c.getField("country_" + isoCode);
+                int id = field.getInt(null);
+                return getResources().getString(id);
+            } catch (Exception e) {
+                Log.e("AddClockActivity", "failure to get country string id", e);
+                return isoCode;
             }
-            final Context context = getActivity();
-            for (TimeZoneInfo timeZone : timeZones) {
-                WorldClock.Clocks.addClock(context, timeZone);
-            }
-            returnResult(timeZones.size());
         }
-
+        
         private void returnResult(int resultCode) {
             getActivity().setResult(resultCode);
             getActivity().finish();
         }
 
         public boolean onQueryTextChange(String newText) {
-            if (!TextUtils.isEmpty(newText)) {
-                mAdapter.getFilter().filter(newText);
-            } else {
-                mAdapter.getFilter().filter(null);
-            }
+            mCurFilter = newText;
+            getLoaderManager().restartLoader(0, null, this);
             return true;
         }
 
@@ -251,51 +315,4 @@ public class AddClockActivity extends SherlockFragmentActivity {
             return false;
         }
     }
-
-    private static class TimeZoneArrayAdapter extends
-            FilterableArrayAdapter<TimeZoneInfo> {
-
-        public TimeZoneArrayAdapter(Context context, TimeZoneInfo[] objects) {
-            super(context, R.layout.time_zone_item, R.id.city_text, objects);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view;
-            if (convertView == null) {
-                view = LayoutInflater.from(getContext()).inflate(
-                        R.layout.time_zone_item, null);
-            } else {
-                view = convertView;
-            }
-            bindView(position, view);
-            return view;
-        }
-
-        private void bindView(int position, View view) {
-            TimeZoneInfo item = getItem(position);
-            TextView cityText = (TextView) view.findViewById(R.id.city_text);
-            cityText.setText(item.getCity());
-            TextView areaText = (TextView) view.findViewById(R.id.area_text);
-            areaText.setText(item.getArea());
-            TextView timeDiffText = (TextView) view.findViewById(R.id.time_diff_text);
-            TimeZone tz = item.getTimeZone();
-            timeDiffText.setText(TimeZoneInfo.getTimeDifferenceString(tz));
-            TextView timeZoneDescText = (TextView) view.findViewById(R.id.timezone_desc_text);
-            timeZoneDescText.setText(TimeZoneInfo.getDescription(tz));
-        }
-
-        @Override
-        protected boolean match(String prefixString, TimeZoneInfo value) {
-            return value.getCity().toLowerCase().contains(prefixString);
-        }
-    }
-    
-    private static class CityComparer implements Comparator<TimeZoneInfo> {
-        @Override
-        public int compare(TimeZoneInfo lhs, TimeZoneInfo rhs) {
-            return lhs.getCity().compareTo(rhs.getCity());
-        }
-    }
-
 }
