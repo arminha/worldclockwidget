326a7e80a9c56e405f62938f8859bbfe9db56421
Armin Häberling <armin.aha@gmail.com>
2012-07-02 16:18:17 +0200

    Fix database update/creation

--- worldclockwidget/src/ch/corten/aha/worldclock/provider/WorldClockDatabase.java
+++ worldclockwidget/src/ch/corten/aha/worldclock/provider/WorldClockDatabase.java
@@ -17,9 +17,10 @@ public class WorldClockDatabase extends SQLiteOpenHelper {
     private static final String DATABASE_UPDATE_2 = 
             "alter table clocks add column use_in_widget integer not null default 1";
     
-    private static final String DATABASE_UPDATE_3 =
-            "alter table clocks add column latitude real not null default 0; " +
-            "alter table clocks add column longitude real not null default 0;";
+    private static final String[] DATABASE_UPDATE_3 = {
+            "alter table clocks add column latitude real not null default 0;",
+            "alter table clocks add column longitude real not null default 0;"
+    };
 
     private static final String DATABASE_NAME = "worldclock";
     private static final int DATABASE_VERSION = 3;
@@ -31,7 +32,9 @@ public class WorldClockDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(DATABASE_CREATE);
-        db.execSQL(DATABASE_UPDATE_3);
+        for (String stmt : DATABASE_UPDATE_3) {
+            db.execSQL(stmt);
+        }
     }
     
     @Override
@@ -40,7 +43,9 @@ public class WorldClockDatabase extends SQLiteOpenHelper {
             db.execSQL(DATABASE_UPDATE_2);
         }
         if (oldVersion < 3 && newVersion >= 3) {
-            db.execSQL(DATABASE_UPDATE_3);
+            for (String stmt : DATABASE_UPDATE_3) {
+                db.execSQL(stmt);
+            }
         }
     }
 }
