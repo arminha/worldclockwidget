1ec1e725c2ae2238f57512857ea1c1b9f8c5b670
Armin Häberling <armin.aha@gmail.com>
2013-01-14 15:11:13 +0100

    Set title and add home button in action bar.

--- worldclockwidget/src/ch/corten/aha/worldclock/WorldClockPreferenceActivity.java
+++ worldclockwidget/src/ch/corten/aha/worldclock/WorldClockPreferenceActivity.java
@@ -16,14 +16,27 @@
 
 package ch.corten.aha.worldclock;
 
-import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
 
-public class WorldClockPreferenceActivity extends PreferenceActivity {
+import android.os.Bundle;
+
+public class WorldClockPreferenceActivity extends SherlockPreferenceActivity {
     @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.world_clock_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
