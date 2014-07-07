b2dd62f621d9ab6c6864c7d1fe65d5d11a9766c2
Armin Häberling <armin.aha@gmail.com>
2014-07-07 23:19:46 +0200

    Add script to retrieve latest city data from geonames.org

---
+++ data/update_city_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+'''
+Download the latest versions of cities15000.txt and countryInfo.txt from
+http://download.geonames.org/export/dump/
+'''
+
+import urllib
+import zipfile
+import sys
+import os
+
+def main():
+    retrieve('countryInfo.txt')
+    citiesZip = 'cities15000.zip'
+    retrieve(citiesZip)
+    unzip(citiesZip)
+
+def retrieve(fileName):
+    printnln('Retrieve %s..' % fileName)
+    urllib.urlretrieve(BASE_URL + fileName, fileName)
+    print(' Done.')
+
+def unzip(fileName):
+    printnln('Unzip %s..' % fileName)
+    with zipfile.ZipFile(fileName, 'r') as zfile:
+        zfile.extractall()
+    os.remove(fileName)
+    print(' Done.')
+
+def printnln(text):
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+BASE_URL = 'http://download.geonames.org/export/dump/'
+
+if __name__ == "__main__":
+    main()
