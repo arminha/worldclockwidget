2402b87ed41ec1b3a991a1d24b0b628869a75f81
Armin Häberling <armin.aha@gmail.com>
2015-10-12 22:52:43 +0200

    Remove unused imports

--- worldclockwidget/src/main/java/ch/corten/aha/worldclock/weather/AndroidWeatherServiceFactory.java
+++ worldclockwidget/src/main/java/ch/corten/aha/worldclock/weather/AndroidWeatherServiceFactory.java
@@ -17,12 +17,6 @@
 package ch.corten.aha.worldclock.weather;
 
 import android.content.Context;
-import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import ch.corten.aha.worldclock.BuildConfig;
 import ch.corten.aha.worldclock.weather.msn.MsnWeatherService;
