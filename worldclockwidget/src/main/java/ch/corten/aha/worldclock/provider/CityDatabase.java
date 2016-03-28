3884c8dd7f69eaafdd63340d39511e4d67189bd1
Armin Häberling <armin.aha@gmail.com>
2016-03-28 19:00:47 +0200

    Update city data

--- worldclockwidget/src/main/java/ch/corten/aha/worldclock/provider/CityDatabase.java
+++ worldclockwidget/src/main/java/ch/corten/aha/worldclock/provider/CityDatabase.java
@@ -48,7 +48,7 @@ class CityDatabase extends SQLiteOpenHelper {
     private static final String DROP_TABLE = "drop table if exists cities";
 
     private static final String DATABASE_NAME = "cities";
-    private static final int DATABASE_VERSION = 16;
+    private static final int DATABASE_VERSION = 17;
 
     private Context mContext;
     private boolean mNeedsVacuum;
